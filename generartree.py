b3afee30fe15b9076989f87e5e46c2fb6922e277
Valeria Sell <valeria_sell@outlook.com>
2021-05-19 22:38:48 -0600

    @param nuevos
    @param de angulo de ramificaciones, profundidad, long inicial.

--- generartree.py
+++ generartree.py
@@ -1,26 +1,33 @@
 ##Fuente: https://rosettacode.org/wiki/Fractal_tree#Python
 import pygame, math
+from pygame.locals import *
 
 pygame.init()
-window = pygame.display.set_mode((600, 600))
+window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Fractal Tree")
 screen = pygame.display.get_surface()
 
 def drawTree(x1, y1, angle, depth):
-    fork_angle = 20
-    base_len = 10.0
+    #angle is upright (-90)
+    #ram_angle es el angulo de las ramificaciones
+    ram_angle = 23
+    init_line_len = 10.0
     if depth > 0:
-        x2 = x1 + int(math.cos(math.radians(angle)) * depth * base_len)
-        y2 = y1 + int(math.sin(math.radians(angle)) * depth * base_len)
+        #?Encontrar donde se define la proporcion de decremento de line en cada nivel
+        x2 = x1 + int(math.cos(math.radians(angle)) * depth * init_line_len)
+        y2 = y1 + int(math.sin(math.radians(angle)) * depth * init_line_len)
         pygame.draw.line(screen, (255,255,255), (x1, y1), (x2, y2),2)
-        drawTree(x2, y2, angle - fork_angle, depth - 1)
-        drawTree(x2, y2, angle + fork_angle, depth - 1)
+        drawTree(x2, y2, angle - ram_angle, depth - 1)
+        drawTree(x2, y2, angle + ram_angle, depth - 1)
 
 def input(event):
     if event.type == pygame.QUIT:
         exit(0)
 
-drawTree(300, 550, -90, 9)
+#drawtree recibe x, y, angulo de posicion y profundidad de ramas
+#!auxilio diferencia entre numero de ramificaciones y los niveles? 
+#!porque el numero de ramif puede ser random?
+drawTree(400, 550, -90, 10)
 pygame.display.flip()
 while True:
     input(pygame.event.wait())
