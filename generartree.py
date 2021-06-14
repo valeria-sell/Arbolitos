493fe7155178863279a308ba5b6625a475748187
Valeria Sell <valeria_sell@outlook.com>
2021-06-14 02:04:56 -0600

    update de genetic.py
    cambios en generartree.py, formato de clase en genetic.py

--- generartree.py
+++ generartree.py
@@ -7,18 +7,16 @@ window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Fractal Tree")
 screen = pygame.display.get_surface()
 
-def drawTree(x1, y1, angle, depth):
+def drawTree(x1, y1, angle, depth, ram_angle, init_line_len): #Falta incluir ram_number
     #angle is upright (-90)
     #ram_angle es el angulo de las ramificaciones
-    ram_angle = 17
-    init_line_len = 10.0
     if depth > 0:
         #?Encontrar donde se define la proporcion de decremento de line en cada nivel
         x2 = x1 + int(math.cos(math.radians(angle)) * depth * init_line_len)
         y2 = y1 + int(math.sin(math.radians(angle)) * depth * init_line_len)
         pygame.draw.line(screen, (255,255,255), (x1, y1), (x2, y2),2)
-        drawTree(x2, y2, angle - ram_angle, depth - 1)
-        drawTree(x2, y2, angle + ram_angle, depth - 1)
+        drawTree(x2, y2, angle - ram_angle, depth - 1, ram_angle, init_line_len)
+        drawTree(x2, y2, angle + ram_angle, depth - 1, ram_angle, init_line_len)
 
 def input(event):
     if event.type == pygame.QUIT:
@@ -27,7 +25,7 @@ def input(event):
 #drawtree recibe x, y, angulo de posicion y profundidad de ramas
 #!auxilio diferencia entre numero de ramificaciones y los niveles? 
 #!porque el numero de ramif puede ser random?
-drawTree(400, 550, -90, 10)
+drawTree(400, 550, -90, 10, 17, 10.0)
 pygame.display.flip()
 while True:
     input(pygame.event.wait())
--- genetic.py
+++ genetic.py
@@ -1,18 +1,59 @@
-#Esqueleto función fitness
-def get_percentage_in(tree, silhouette):
-    #buenas aqui encuentra porcentaje de pixeles del arb_ind que coinciden con silueta
-    value = 0
-    return value
+from generartree import drawTree
 
-def get_percentage_out(tree, silhouette):
-    #buenas aqui encuentra porcentaje de pixeles del arb_ind que no coinciden con silueta
-    value = 0
-    return value
+SILHOUETTE = 0
+X1 = 400
+Y1 = 550
+ANGLE = -90 
 
-def calc_fitness(tree, silhouette):
-    #! PENDIENTE, PENALIZAR EL TAMAÑO SI ES DEMASIADO PEQUEÑO 
-    percentage_in= get_percentage_in(tree, silhouette)
-    percentage_out= get_percentage_out(tree, silhouette)
+class Individual:
+    def __init__(self, fitness, depth, init_line_len, decrease_prop, ram_number, ram_angle):
+        self.fitness = fitness
+        self.depth = depth
+        self.init_line_len = init_line_len
+        self.decrease_prop = decrease_prop #pendiente de agregar a drawTree
+        self.ram_number = ram_number #pendiente de agregar a drawTree
+        self.ram_angle = ram_angle
+    
+    def printTree(self):
+        drawTree(X1, Y1, ANGLE, depth, ram_angle, init_line_len)
 
-    fitness_value = ( percentage_in - percentage_out ) / 100
-    return fitness_value
+    def get_percentage_in(self, SILHOUETTE):
+        #buenas aqui encuentra porcentaje de pixeles del arb_ind que coinciden con silueta
+        value = 0
+        return value
+
+    def get_percentage_out(self, SILHOUETTE):
+        #buenas aqui encuentra porcentaje de pixeles del arb_ind que no coinciden con silueta
+        value = 0
+        return value
+
+    def calc_fitness(self, SILHOUETTE): #fitness function
+        #! PENDIENTE, PENALIZAR EL TAMAÑO SI ES DEMASIADO PEQUEÑO, qué tan largo es el alcance? 
+        # EXTREMOS, probar con eso para ver que tanto esta distribuido
+        percentage_in= get_percentage_in(self, SILHOUETTE)
+        percentage_out= get_percentage_out(self, SILHOUETTE)
+
+        fitness_value = ( percentage_in - percentage_out ) / 100
+        return fitness_value
+
+    def selection(self): #Individual generation[]
+        best_fit = 0
+        return best_fit 
+
+    def mutate(self):
+        mutation = True
+
+    def crossover(self):
+        newInd = 0
+        return newInd
+
+    #def do_generation():
+    #def print_individual():
+
+
+
+
+
+
+
+
